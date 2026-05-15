96cc20694cbfe026454301aeea68fa019583b57a
Kavya <kavyakumar2@gmail.com>
2026-05-15 21:54:02 +0530

    Episode 2

--- app.js
+++ app.js
@@ -1,18 +1,20 @@
-
+import * as React from "react";
+import { createRoot } from "react-dom/client";
 const heading = React.createElement("h1", {
    id: "heading"
 }, "Hello From React!");
-const root = ReactDOM.createRoot(document.getElementById("root"));
-// root.render(heading)
+const root = createRoot(document.getElementById("root"));
 
-const parent = React.createElement("div", { id: "parent" },
-   [React.createElement("h1", { id: "child" }, "Namaste React"),
-   React.createElement("h2", { id: "child2" }, "Namaste React 2"),
-   React.createElement("h3", { id: "child3" }, "Namaste React 3"),
-
-   ]
+const parent = React.createElement(
+  "div",
+  { id: "parent" },
+  [
+    React.createElement("h1", { id: "child", key: "c1" }, "Namaste React"),
+    React.createElement("h2", { id: "child2", key: "c2" }, "Namaste React 2"),
+    React.createElement("h3", { id: "child3", key: "c3" }, "Namaste React 3"),
+  ]
 );
 
-root.render(parent)
+root.render(parent);
 
 
